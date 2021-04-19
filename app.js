322c11094c5fee5056592bf4125ddde34ea2e6a5
ginnnnnn <aginlo@hotmail.com>
2021-04-19 11:25:29 +0800

    add env and recreate table

--- app.js
+++ app.js
@@ -1,87 +1,143 @@
+require("dotenv").config();
 const amqp = require("amqplib");
 const { Client } = require("pg");
-var q = "import_images";
+
+const {
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+  BROKER_IP,
+  BROKER_PORT,
+  BROKER_QUEUE,
+} = process.env;
+
+const preCreateQuery = `CREATE TABLE if NOT EXISTS image_folders(
+  id serial PRIMARY KEY,
+  folder VARCHAR,
+  origin VARCHAR,
+  create_at TIMESTAMP,
+  origin_folder VARCHAR UNIQUE
+  ) ;
+  
+  CREATE TABLE if NOT EXISTS images(
+  id serial PRIMARY KEY,
+  filename VARCHAR,
+  import_at TIMESTAMP,
+  url VARCHAR(200) UNIQUE,
+  folder_id INTEGER REFERENCES image_folders(id) ON DELETE CASCADE
+  );`;
+
+const handleTablePreCreate = async () => {
+  const client = new Client({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+  });
+  await client.connect();
+  await client.query(preCreateQuery);
+  await client.end();
+};
 
 //event function
 async function handleAddDir(client, result, ackMsg) {
+  const { folderPath, origin } = result;
   try {
     await client.connect();
-    const checkQuery = "SELECT * FROM image_folders WHERE path = $1";
-    const checkRes = await client.query(checkQuery, [result.folderPath]);
-    if (checkRes.rows.length > 0) {
-      throw new Error("Dir exist");
-    }
-    const queryText =
-      "INSERT INTO image_folders( path, create_at) VALUES($1 , $2)";
-    await client.query(queryText, [result.folderPath, new Date()]);
+    const queryText = `INSERT INTO image_folders(
+        folder, 
+        origin, 
+        create_at , 
+        origin_folder) 
+        VALUES($1,$2,$3,$4 ) RETURNING id`;
+    await client.query(queryText, [
+      folderPath,
+      origin,
+      new Date(),
+      `${origin}/${folderPath}`,
+    ]);
     await client.end();
     ackMsg();
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     console.log(result);
     await client.end();
     ackMsg();
   }
 }
 async function handleUnlinkDir(client, result, ackMsg) {
+  const { folderPath, origin } = result;
+  const originFolder = `${origin}/${folderPath}`;
   try {
     await client.connect();
-    const queryText = "DELETE FROM image_folders WHERE path = $1";
-    await client.query(queryText, [result.folderPath]);
+    const queryText = "DELETE FROM image_folders WHERE origin_folder = $1";
+    await client.query(queryText, [originFolder]);
     await client.end();
     ackMsg();
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     console.log(result);
     ackMsg();
   }
 }
 
 async function handleAddImage(client, result, ackMsg) {
+  const { folderPath, origin, filename } = result;
+  const originFolder = `${origin}/${folderPath}`;
+  const url = `${originFolder}/${filename}`;
   try {
     await client.connect();
-    const checkFolderQuery = "SELECT * FROM image_folders WHERE path = $1";
-    const checkFolderRes = await client.query(checkFolderQuery, [
-      result.folderPath,
-    ]);
+    //check if folder exist
+    const checkFolderQuery =
+      "SELECT id FROM image_folders WHERE origin_folder = $1";
+    let id;
+    const checkFolderRes = await client.query(checkFolderQuery, [originFolder]);
     //folder not exist
+
     if (checkFolderRes.rows.length <= 0) {
-      const addFolderText =
-        "INSERT INTO image_folders( path, create_at) VALUES($1 , $2)";
-      await client.query(addFolderText, [result.folderPath, new Date()]);
+      //add folder
+      const addFolderQueryText = `INSERT INTO image_folders(
+        folder, 
+        origin, 
+        create_at , 
+        origin_folder) 
+        VALUES($1,$2,$3,$4 ) RETURNING id`;
+      const res = await client.query(addFolderQueryText, [
+        folderPath,
+        origin,
+        new Date(),
+        originFolder,
+      ]);
+      id = res.rows[0]["id"];
+    } else {
+      id = checkFolderRes.rows[0]["id"];
     }
-    const queryText =
-      "SELECT * FROM images WHERE filename = $1 AND folder_path = $2";
-    const checkRes = await client.query(queryText, [
-      result.filename,
-      result.folderPath,
-    ]);
-    if (checkRes.rows.length > 0) {
-      //file exist
-      throw new Error("file exist");
-    }
-    const insertText =
-      "INSERT INTO images (filename,import_at,folder_path) VALUES($1,$2,$3)";
-    const res = await client.query(insertText, [
-      result.filename,
-      new Date(),
-      result.folderPath,
-    ]);
+    const insertText = `INSERT INTO images (
+      filename,
+      import_at,
+      url,
+      folder_id) VALUES($1,$2,$3,$4)`;
+    await client.query(insertText, [filename, new Date(), url, id]);
     await client.end();
     ackMsg();
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     console.log(result);
     await client.end();
     ackMsg();
   }
 }
 async function handleUnlinkImage(client, result, ackMsg) {
+  const { folderPath, origin, filename } = result;
+  const originFolder = `${origin}/${folderPath}`;
+  const url = `${originFolder}/${filename}`;
   try {
     await client.connect();
-    const queryText =
-      "DELETE FROM images WHERE filename = $1 AND folder_path = $2";
-    await client.query(queryText, [result.filename, result.folderPath]);
+    const queryText = "DELETE FROM images WHERE url = $1";
+    await client.query(queryText, [url]);
     await client.end();
     ackMsg();
   } catch (err) {
@@ -96,23 +152,24 @@ async function handleUnlinkImage(client, result, ackMsg) {
 
 async function connect() {
   try {
-    const connection = await amqp.connect("amqp://localhost:5672");
+    const connection = await amqp.connect(`amqp://${BROKER_IP}:${BROKER_PORT}`);
     var ch = await connection.createChannel();
     // if (err != null) bail(err);
     ch.prefetch(2);
-    await ch.assertQueue("import_images");
-    ch.consume(q, function (msg) {
+    await ch.assertQueue(BROKER_QUEUE);
+    await handleTablePreCreate();
+    ch.consume(BROKER_QUEUE, function (msg) {
       if (msg !== null) {
         const result = JSON.parse(msg.content.toString());
         const ackMsg = () => {
           return ch.ack(msg);
         };
         const client = new Client({
-          user: "postgres",
-          host: "localhost",
-          database: "aimodel",
-          password: "postgres",
-          port: 5432,
+          user: DB_USER,
+          host: DB_HOST,
+          database: DB_NAME,
+          password: DB_PASSWORD,
+          port: DB_PORT,
         });
         if (result.event === "addDir") {
           handleAddDir(client, result, ackMsg);
